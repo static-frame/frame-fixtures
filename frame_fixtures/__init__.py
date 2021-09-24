3b7e77f23207ed077155ab00f6dc5fbc521a3f99
Christopher Ariza <ariza@flexatone.com>
2021-09-23 19:57:56 -0700

    updated version, mypy

--- frame_fixtures/__init__.py
+++ frame_fixtures/__init__.py
@@ -1,5 +1,5 @@
 from frame_fixtures.core import Fixture as Fixture #pylint: disable=W0611
 from frame_fixtures.core import parse as parse #pylint: disable=W0611
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
