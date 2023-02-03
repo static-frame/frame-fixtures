9307f0d7e6ec3ab357116b38a7f600f28aaf527f
Christopher Ariza <ariza@flexatone.com>
2023-02-03 11:03:31 -0800

    1.0.0 RC 1

--- frame_fixtures/__init__.py
+++ frame_fixtures/__init__.py
@@ -1,6 +1,6 @@
 from frame_fixtures.core import Fixture as Fixture #pylint: disable=W0611
 from frame_fixtures.core import parse as parse #pylint: disable=W0611
 
-__version__ = '0.3.0'
+__version__ = '1.0.0'
 
 
